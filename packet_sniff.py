7752acc0b61346bf2a48f2af4655bcbbee5c9d67
Kylie Angel <77999097+kyliea5728@users.noreply.github.com>
2024-05-05 16:56:03 -0400

    Add files via upload

--- packet_sniff.py
+++ packet_sniff.py
@@ -1,16 +1,17 @@
+#run as sudo python packet_sniff.py
 import socket
 import struct
 import textwrap
 
-TAB_1 '\t - '
-TAB_2 '\t\t - '
-TAB_3 '\t\t\t - '
-TAB_4 '\t\t\t\t - '
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
 
-DATA_TAB_1 '\t '
-DATA_TAB_2 '\t\t '
-DATA_TAB_3 '\t\t\t '
-DATA_TAB_4 '\t\t\t\t '
+DATA_TAB_1 = '\t '
+DATA_TAB_2 = '\t\t '
+DATA_TAB_3 = '\t\t\t '
+DATA_TAB_4 = '\t\t\t\t '
 
 
 def main():
@@ -20,12 +21,31 @@ def main():
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print('\nEthernet Frame: ')
-        print('Destination: {}, Source {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+        print(TAB_1 + 'Destination: {}, Source {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
         
         #8 for IPv4
         if eth_proto == 8:
             (version, header_length, ttl, proto, src, target, data) = ipv4_packet(data)
             print(TAB_1 + 'IPv4 Packet: ')
+            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {}'.format(version, header_length, ttl)) 
+            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
+            
+            #17 for UDP
+            if eth_proto == 17:
+                src_port, dest_port, size, data = udp_segment(data)
+                print(TAB_1 + 'UDP Packet: ')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Size: {}'.format(src_port, dest_port, size)) 
+                print(TAB_2 + 'Data: {}'.format(data))
+            
+            #6 fpr TCP
+            elif proto == 6:
+                (src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, falg_rst, flag_syn. flag_fin) = tcp_segment(data)
+                print(TAB_1 + 'TCP Segment: ')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(src_port, dest_port))
+                print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(sequence, acknowledgement))
+                print(TAB_2 + 'Flags: ')
+                print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg, flag_ack, flag_psh, falg_rst, flag_syn. flag_fin))
+                print(TAB_2 + 'Data: {}'.format(data))
 
 #unpack ethernet frame
 #Grab first 14 bytes and unpack it (contains dest. and source info)
@@ -44,8 +64,8 @@ def ipv4_packet(data):
     version_header_length = data[0]
     version = version_header_length >> 4
     header_length = (version_header_length & 15) * 4
-    ttl, proto. src, target = struct.unpack('! 8x B B 2x 4s 4s', data[0:20])
-    return version, header_length, ttl, proto, ipv4(src), ivp4(target), data[header_length:]
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[0:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
     
 #Returns properly formatted IPv4 address
 def ipv4(addr):
@@ -57,5 +77,16 @@ def udp_segment(data):
     src_port, dest_port, size = struct.unpack('! H H 2x H', data[0:8])
     return src_port, dest_port, size, data[8:]
     
+#Unpacks TCP data
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin
 
 main()
